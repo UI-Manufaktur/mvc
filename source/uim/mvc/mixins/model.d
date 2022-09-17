1b8cffb8a7693c18cf1b8868606cb0047b3d42df
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-09-17 12:41:19 +0200

    Initial works

--- source/uim/mvc/mixins/model.d
+++ source/uim/mvc/mixins/model.d
@@ -1,24 +1,2 @@
 public import uim.mvc.mixins.model;
 
-@safe:
-import uim.mvc;
-
-class DMVCModel : IMVCModel { 
-    this() { initialize; } 
-    this(DMVCApplication myApplication) { this().application(myApplication); } 
-    this(string myName) { this().name(myName); } 
-    this(STRINGAA myOptions) { this().options(myOption); } 
-
-    this(DMVCApplication myApplication, string myName) { this(myApplication).name(myName); } 
-    this(DMVCApplication myApplication, STRINGAA myOptions) { this(myApplication).options(myOption); } 
-    this(DMVCApplication myApplication, string myName, STRINGAA myOptions) { this(myApplication, myName).options(myOption); } 
-
-    this(string myName, STRINGAA myOptions) { this(myName).options(myOption); } 
-
-    mixin(OProperty!("STRINGAA", "parameters"));
-}
-mixin(MVCModelCalls!("MVCModel", "DMVCModel"));
-
-version(test_uim_mvc) { unitest { 
-    assert(MVCModel);
-}} 
--- source/uim/mvc/models/model.d
+++ source/uim/mvc/models/model.d
@@ -2,3 +2,25 @@ module uim.mvc.models;
 
 @safe:
 import uim.mvc;
+
+class DMVCModel : IMVCModel { 
+  this() { initialize; } 
+  this(DMVCApplication myApplication) { this().application(myApplication); } 
+  this(string myName) { this().name(myName); } 
+  this(STRINGAA myOptions) { this().options(myOption); } 
+
+  this(DMVCApplication myApplication, string myName) { this(myApplication).name(myName); } 
+  this(DMVCApplication myApplication, STRINGAA myOptions) { this(myApplication).options(myOption); } 
+  this(DMVCApplication myApplication, string myName, STRINGAA myOptions) { this(myApplication, myName).options(myOption); } 
+
+  this(string myName, STRINGAA myOptions) { this(myName).options(myOption); } 
+
+  mixin(OProperty!("STRINGAA", "parameters"));
+
+  void initialize() {}
+}
+mixin(MVCModelCalls!("MVCModel", "DMVCModel"));
+
+version(test_uim_mvc) { unitest { 
+  assert(MVCModel);
+}} 
