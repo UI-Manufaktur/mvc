70462e481a46a8e65972c8416c0366bb07ac3617
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-09-17 12:40:17 +0200

    Initial works

--- source/uim/mvc/views/view.d
+++ source/uim/mvc/views/view.d
@@ -4,21 +4,23 @@ module uim.mvc.views.view;
 import uim.mvc;
 
 class DMVCView : IMVCView { 
-    this() { initialize; } 
-    this(DMVCApplication myApplication) { this().application(myApplication); } 
-    this(string myName) { this().name(myName); } 
-    this(STRINGAA myOptions) { this().options(myOption); } 
+  this() { initialize; } 
+  this(DMVCApplication myApplication) { this().application(myApplication); } 
+  this(string myName) { this().name(myName); } 
+  this(STRINGAA myOptions) { this().options(myOption); } 
 
-    this(DMVCApplication myApplication, string myName) { this(myApplication).name(myName); } 
-    this(DMVCApplication myApplication, STRINGAA myOptions) { this(myApplication).options(myOption); } 
-    this(DMVCApplication myApplication, string myName, STRINGAA myOptions) { this(myApplication, myName).options(myOption); } 
+  this(DMVCApplication myApplication, string myName) { this(myApplication).name(myName); } 
+  this(DMVCApplication myApplication, STRINGAA myOptions) { this(myApplication).options(myOption); } 
+  this(DMVCApplication myApplication, string myName, STRINGAA myOptions) { this(myApplication, myName).options(myOption); } 
 
-    this(string myName, STRINGAA myOptions) { this(myName).options(myOption); } 
+  this(string myName, STRINGAA myOptions) { this(myName).options(myOption); } 
 
-    mixin(OProperty!("STRINGAA", "parameters"));
+  void initialize() {}
+  
+  mixin(OProperty!("STRINGAA", "parameters"));
 }
 mixin(MVCViewCalls!("MVCView", "DMVCView"));
 
 version(test_uim_mvc) { unitest { 
-    assert(MVCView);
+  assert(MVCView);
 }} 
