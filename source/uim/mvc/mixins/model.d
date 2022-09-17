91b1cde73a0633133f68d5f1eaaa3ce0934aa481
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-09-17 12:33:47 +0200

    Initial works

--- source/uim/mvc/mixins/model.d
+++ source/uim/mvc/mixins/model.d
@@ -1,1 +1,8 @@
 public import uim.mvc.mixins.model;
+
+class DMVCModel : IMVCModel { 
+    this() { initialize; } 
+    this(DMVCApplication myApplication) { this().application(myApplication); } 
+    this(string myName) { this().name(myName); } 
+    this(STRINGAA myOptions) { this().options(myOption); } 
+}
