0760d0e699458b7eaa8414d05fcf3499f96c212a
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-01-27 15:09:15 +0100

    Consolidating uim-mvc

--- source/uim/baklava/views/components/tables/components/rows/package.d
+++
@@ -1,9 +0,0 @@
-module uim.baklava.views.components.tables.components.rows;
-
-@safe:
-import uim.baklava;
-
-// Main
-public import uim.baklava.views.components.tables.components.rows.row;
-
-public import uim.baklava.views.components.tables.components.rows.entity;
--- source/uim/baklava/views/controls/package.d
+++
@@ -1,3 +0,0 @@
-module uim.baklava.views.controls;
-
-public import uim.baklava.views.controls.buttons;
