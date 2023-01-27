75f74026802171f74361e7cc80e2215a33422dce
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-01-27 15:17:04 +0100

    Consolidating uim-mvc

--- source/uim/mvc/views/components/forms/components/footers/package.d
+++ source/uim/mvc/views/components/forms/components/footers/package.d
@@ -1,7 +1,9 @@
+/*********************************************************************************************************
+	Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+	Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+**********************************************************************************************************/
 module uim.mvc.views.components.forms.components.footers;
-
-@safe:
-import uim.mvc;
 
 // Main 
 public import uim.mvc.views.components.forms.components.footers.footer;
