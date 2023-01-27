8c91fa0a35f4910856b3208fcf7993805c70987f
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-01-27 15:07:58 +0100

    Consolidating uim-mvc

--- source/uim/baklava/views/components/navbars/package.d
+++
@@ -1,5 +0,0 @@
-module uim.baklava.views.components.navbars;
-
-public import uim.baklava.views.components.navbars.navbar;
-public import uim.baklava.views.components.navbars.navigation;
-public import uim.baklava.views.components.navbars.slot;
