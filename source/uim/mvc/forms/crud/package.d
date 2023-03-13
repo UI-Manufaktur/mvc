fcb21b40d66a07e24661ab85169a6040d6d8e9f2
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-13 06:34:44 +0100

    Change namespace for form

--- source/uim/mvc/forms/crud/package.d
+++ source/uim/mvc/forms/crud/package.d
@@ -3,21 +3,21 @@
 	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
 	Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
 **********************************************************************************************************/
-module uim.mvc.views.forms.crud;
+module uim.mvc.forms.crud;
 
 @safe:
 import uim.mvc;
 
 // Main
-public import uim.mvc.views.forms.crud.element;
-public import uim.mvc.views.forms.crud.elements;
+public import uim.mvc.forms.crud.element;
+public import uim.mvc.forms.crud.elements;
 
-public import uim.mvc.views.forms.crud.entity;
-public import uim.mvc.views.forms.crud.entities;
+public import uim.mvc.forms.crud.entity;
+public import uim.mvc.forms.crud.entities;
 
 // Modules
-public import uim.mvc.views.forms.crud.create;
-public import uim.mvc.views.forms.crud.read;
-public import uim.mvc.views.forms.crud.update;
-public import uim.mvc.views.forms.crud.delete_;
-public import uim.mvc.views.forms.crud.list;
+public import uim.mvc.forms.crud.create;
+public import uim.mvc.forms.crud.read;
+public import uim.mvc.forms.crud.update;
+public import uim.mvc.forms.crud.delete_;
+public import uim.mvc.forms.crud.list;
---
+++ source/uim/mvc/forms/schemas/package.d
@@ -0,0 +1,5 @@
+module uim.mvc.forms.schemas;
+
+public { // Main
+  import uim.mvc.forms.schemas.schema;
+}
