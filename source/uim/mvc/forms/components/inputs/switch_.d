1962634668e64f796a651409b01c326d5196c64e
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-13 06:34:42 +0100

    Change namespace for form

--- source/uim/mvc/forms/components/inputs/switch_.d
+++ source/uim/mvc/forms/components/inputs/switch_.d
@@ -3,7 +3,7 @@
 	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
 	Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
 **********************************************************************************************************/
-module uim.mvc.views.forms.components.inputs.switch_;
+module uim.mvc.forms.components.inputs.switch_;
 
 @safe:
 import uim.mvc;
