7c5214aa0bb3c2f651e2d8967586e7da76a238d4
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-15 13:31:47 +0100

    Bugfixing

--- source/uim/mvc/routers/package.d
+++ source/uim/mvc/routers/package.d
@@ -8,8 +8,4 @@ module uim.mvc.routers;
 @safe:
 import uim.mvc;
 
-public import uim.mvc.routers.login;
-public import uim.mvc.routers.login2;
-public import uim.mvc.routers.logout;
-public import uim.mvc.routers.register;
 public import uim.mvc.routers.sites;
