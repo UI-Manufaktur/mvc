2ac14e5514c9b9422b01ebb8814a3b99d8225af6
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-16 11:38:19 +0100

    Change in Authors

--- source/uim/mvc/consoles/console.d
+++ source/uim/mvc/consoles/console.d
@@ -1,13 +1,14 @@
 /*********************************************************************************************************
 	Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
 	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
-	Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+	Authors: Ozan Nurettin Süel, mailto:ons@sicherheitsschmiede.de                                                      
 **********************************************************************************************************/
 module uim.mvc.consoles.console;
 
 @safe:
 import uim.mvc;
 
-class DConsole {
-
+class DConsole : DMVCObject, IConsole {
+	mixin(ConsoleThis!("Console"));
 }
+mixin(ConsoleCalls!("Console"));
