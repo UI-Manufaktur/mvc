9f78ca6d7402abf015e3be02b5beee28a58652cc
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-16 11:40:01 +0100

    Change in Authors

---
+++ source/uim/mvc/mixins/console.d
@@ -0,0 +1,30 @@
+module uim.mvc.mixins.console;
+
+@safe:
+import uim.mvc;
+
+@safe:
+import uim.mvc;
+
+auto consoleThis(string name) {
+  return 
+    mvcObjectThis(name)~
+  `
+  `;
+}
+
+template ConsoleThis(string name) {
+  const char[] ConsoleThis = consoleThis(name);
+}
+
+string consoleCalls(string shortName, string className = null) {
+  auto clName = className.length > 0 ? className : "D"~shortName;
+  return
+    mvcObjectCalls(shortName, className)~
+    `
+    `;
+}
+
+template ConsoleCalls(string shortName, string className = null) {
+  const char[] ConsoleCalls = consoleCalls(shortName, className);
+}
