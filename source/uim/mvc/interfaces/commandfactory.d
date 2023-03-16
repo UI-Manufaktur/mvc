5d7a1a3a2105539a2b122465ca4ea62f357310ea
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-16 11:39:57 +0100

    Change in Authors

--- source/uim/mvc/interfaces/commandfactory.d
+++ source/uim/mvc/interfaces/commandfactory.d
@@ -5,11 +5,6 @@ import uim.mvc;
 
 // An interface for abstracting creation of command and shell instances.
 interface ICommandFactory {
-  /**
-    * The factory method for creating Command and Shell instances.
-    *
-    * className - Command/Shell class name.
-    * returns command or shell
-    */
-  ICommand create(string className);
+    //The factory method for creating Command and Shell instances.
+    ICommand create(string className); // className = Command/Shell class name.
 }
